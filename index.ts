f0b5697e92692721ffff790c09cf441f3a6debb8
muntaha-194 <160383120+muntaha-194@users.noreply.github.com>
2024-04-01 14:51:19 +0500

    publishing a simple-calculator

---
+++ index.ts
@@ -0,0 +1,35 @@
+#! /usr/bin/env node
+// SHABANG 
+
+import inquirer from "inquirer";
+
+// just a welcome message
+console.log("WELCOME TO MUNTAHA'S CLI CALCULATOR")
+
+const answer = await inquirer.prompt([
+  { message: "Enter your first number", type: "number", name: "firstNumber" },
+  { message: "Enter your Second number", type: "number", name: "secondNumber" },
+  {
+    message: "Select one of the operator to perform action!",
+    type: "list",
+    name: "operator",
+    choices: ["Addition", "Subtraction", "Division", "Multiplication"],
+  },
+]);
+
+console.log(answer);
+
+// if-else Statements
+if (answer.operator === "Addition") {
+  console.log(answer.firstNumber + answer.secondNumber);
+} else if (answer.operator === "Subtraction") {
+  console.log(answer.firstNumber - answer.secondNumber);
+} else if (answer.operator === "Division") {
+  console.log(answer.firstNumber / answer.secondNumber);
+} else if (answer.operator === "Multiplication") {
+  console.log(answer.firstNumber * answer.secondNumber);
+} else {
+  console.log("please select valid operator");
+}
+
+console.log("THE END");
